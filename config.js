723813746c10a758377342ecc5587ba3ae0ac82b
Crisballng18-web <cnabarrogomes1806089@gmail.com>
2026-09-24 23:42:07 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+/* ------------------------------------------------------------------
+   Lo único que hay que cambiar de todo el proyecto.
+
+   Pega entre las comillas la dirección de tu aplicación web de Apps
+   Script: la larga que termina en /exec.
+
+   Ejemplo:
+   var CONFIG_API = 'https://script.google.com/macros/s/AKfycbxxxxxxxxxxxx/exec';
+   ------------------------------------------------------------------ */
+
+var CONFIG_API = 'https://script.google.com/macros/s/AKfycbwJSjLo-kjMFt-10C3gvny5hgsueYgwI1zCrcclYjU5L3VpUxihWuy2huzrCyVf1Glw/exec';
